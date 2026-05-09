3d660953022838b820784df01ffeb8ea8fd630df
Bahid <bahid@example.com>
2026-05-09 16:15:30 +0200

    Migrate to Postgres

--- prisma/migrations/20260504125451_add_premium_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Package" ADD COLUMN "carrier" TEXT;
-ALTER TABLE "Package" ADD COLUMN "clientName" TEXT;
-ALTER TABLE "Package" ADD COLUMN "clientPhone" TEXT;
-ALTER TABLE "Package" ADD COLUMN "destination" TEXT;
